8d3b178e8a4bc56af3de78a7bbef0ff119f4391c
xTrayambak <xtrayambak@gmail.com>
2026-01-14 12:39:18 +0530

    fix: arith: code cleanups, remove unnecessary calls

--- src/overdrive/arith.nim
+++ src/overdrive/arith.nim
@@ -1,56 +1,55 @@
 ## Accelerated arithmetic operations
 ##
-## Copyright (C) 2025 Trayambak Rai (xtrayambak at disroot dot org)
+## Copyright (C) 2025-2026 Trayambak Rai (xtrayambak at disroot dot org)
 import pkg/overdrive/[flags, types]
 import pkg/shakar
 
 template addVecAvx2Impl[U: Vectorizable](a, b: Vector[U]): Vector[U] =
-  var vec: Vector[U]
-  case a.size
-  of 1:
-    # uint8 (32x u8)
-    vec.reg = mm256_add_epi8(a.reg, b.reg)
-  of 2:
-    # uint16 (16x u16)
-    vec.reg = mm256_add_epi16(a.reg, b.reg)
-  of 4:
-    # uint32 (8x u32)
-    vec.reg = mm256_add_epi32(a.reg, b.reg)
-  of 8:
-    # uint64 (4x u64)
-    vec.reg = mm256_add_epi64(a.reg, b.reg)
-  else:
-    unreachable
-
-  ensureMove(vec)
+  Vector[U](
+    reg:
+      case a.size
+      of 1:
+        # uint8 (32x u8)
+        mm256_add_epi8(a.reg, b.reg)
+      of 2:
+        # uint16 (16x u16)
+        mm256_add_epi16(a.reg, b.reg)
+      of 4:
+        # uint32 (8x u32)
+        mm256_add_epi32(a.reg, b.reg)
+      of 8:
+        # uint64 (4x u64)
+        mm256_add_epi64(a.reg, b.reg)
+      else:
+        unreachable
+        M256i()
+  )
 
 template addVecSseImpl[U: Vectorizable](a, b: Vector[U]): Vector[U] =
-  var vec: Vector[U]
-  case vec.size
-  of 1:
-    # uint8 (32x u8)
-    vec.reg = mm_add_epi8(a.reg, b.reg)
-  of 2:
-    # uint16 (16x u16)
-    vec.reg = mm_add_epi16(a.reg, b.reg)
-  of 4:
-    # uint32 (8x u32)
-    vec.reg = mm_add_epi32(a.reg, b.reg)
-  of 8:
-    # uint64 (4x u64)
-    vec.reg = mm_add_epi64(a.reg, b.reg)
-  else:
-    unreachable
+  Vector[U](
+    reg:
+      case a.size
+      of 1:
+        # uint8 (16x u8)
+        mm_add_epi8(a.reg, b.reg)
+      of 2:
+        # uint16 (8x u16)
+        mm_add_epi16(a.reg, b.reg)
+      of 4:
+        # uint32 (4x u32)
+        mm_add_epi32(a.reg, b.reg)
+      of 8:
+        # uint64 (2x u64)
+        mm_add_epi64(a.reg, b.reg)
+      else:
+        unreachable
+        M128i()
+  )
 
-  ensureMove(vec)
-
-func addImpl[U: Vectorizable](a, b: Vector[U]): Vector[U] {.inline.} =
+func `+`*[U: Vectorizable](a, b: Vector[U]): Vector[U] {.inline.} =
   when hasAvx2:
     addVecAvx2Impl(a, b)
   elif hasSse2:
     addVecSseImpl(a, b)
   else:
     unreachable
-
-func `+`*[U: Vectorizable](a, b: Vector[U]): Vector[U] {.inline.} =
-  addImpl(a, b)
